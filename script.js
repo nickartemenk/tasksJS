6ba536f99b7732ae83fadfbb736ef7450cc6fc9b
nickartemenk <artemenkokit@gmail.com>
2024-10-04 22:32:20 +0300

    1 and 2 task

--- script.js
+++ script.js
@@ -4,8 +4,6 @@
 // message = ('Я сообщение!')
 // console.log(message)
 
-
-
 // let admin;
 // let name;
 //
@@ -15,8 +13,6 @@
 //
 // alert(admin)
 
-
-
 // let name = prompt("Какое ваше имя?", "")
 //
 // alert(`Ваше имя: ${name}`)
@@ -25,8 +21,6 @@
 // value = String(value);
 //
 // alert(typeof value)
-
-
 
 // let conv = ("4" / "2");
 //
@@ -41,37 +35,86 @@
 //
 // console.log(x, y);
 
-
 // let a = prompt("Первое число?", 1);
 // let b = prompt("Второе число?", 2);
 //
 // alert(+a + +b);
 
-const persone = {
-  name: `Alex`,
-  tel: `+375295555555`,
-  parents: {
-    mom:`Olga`,
-    dad:`Char`
+// const persone = {
+//   name: `Alex`,
+//   tel: `+375295555555`,
+//   parents: {
+//     mom:`Olga`,
+//     dad:`Char`
+//   }
+// };
+
+// const clone = JSON.parse(JSON.stringify(persone));
+
+// clone.parents.mom = `Anna`;
+// console.log(persone);
+// console.log(clone);
+
+
+
+
+
+
+
+/*1 ЗАДАЧА 1 ВАРИАНТ*/
+
+
+const fruits = ['kiwi', 'apple', 'kiwi', 'orange', 'kiwi', 'apple'];
+
+const countFruits = (arr) =>{
+  const result = {};
+  
+  for (const frut of arr) {
+    if (!result[frut]) {
+      result[frut] = 1;
+    } else {
+      result[frut]++;
+    }
   }
-};
+  
+  return result;
+}
 
-const clone = JSON.parse(JSON.stringify(persone));
+console.log(countFruits(fruits)); 
 
-clone.parents.mom = `Anna`;
-console.log(persone);
-console.log(clone);
+/*1 ЗАДАЧА 2 ВАРИАНТ*/
+
+const fruits = ['kiwi', 'apple', 'kiwi', 'orange', 'kiwi', 'apple'];
+
+const countFruits = (arr) => {
+  const result = {};
+  
+  arr.forEach(fruit => {
+    result[fruit] = (result[fruit] || 0) + 1;
+  });
+  
+  return result;
+}
+
+console.log(countFruits(fruits)); 
 
 
+/*2 ЗАДАЧА */
 
+const fruits = ['kiwi', 'apple', 'kiwi', 'orange', 'kiwi', 'apple'];
 
+const duplicates = (arr) => {
+    const uniqueFruits = [];
+    const currentFr = [];
 
+    for (let fruit of arr) {
+        if (!currentFr.includes(fruit)) {
+            uniqueFruits.push(fruit);
+            currentFr.push(fruit);
+        }
+    }
 
+    return uniqueFruits;
+}
 
-
-
-
-
-
-
-
+console.log(duplicates(fruits));
