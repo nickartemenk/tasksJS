7485a98a3078a4e5c7736a9081fc822822557e1a
nickartemenk <artemenkokit@gmail.com>
2024-11-29 17:16:54 +0300

    rev pizza

--- 6-task.js
+++ 6-task.js
@@ -1,8 +1,10 @@
 const myStr = "pizza";
-let reverseStr = "";
+// let reverseStr = "";
 
-for (let letter of myStr) {
-  reverseStr = letter + reverseStr;
-}
+// for (let letter of myStr) {
+//   reverseStr = letter + reverseStr;
+// }
+
+const reverseStr = myStr.split("").reverse();
 
 console.log(reverseStr);
